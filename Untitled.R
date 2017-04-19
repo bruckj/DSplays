9e98e241b9247d3d7f5e500ae330b7ac5ce14938
Bruck <jozsef.bruck@vodafone.com>
2017-04-20 00:49:02 +0200

    version 1.0 (but only internal use)

--- Untitled.R
+++ Untitled.R
@@ -19,30 +19,56 @@ std <- function(x){
         return(std)
 }
 
-std(v) - sd(v)
-
-mean(v)==mea(v)
 
 
 confintmean <- function(v,level){
-        msamp <- NA
-        for (i in 1:1000) {
-                samp <- sample(v,round(length(v)/1000))
-                msamp[i] <- mea(samp) 
-        }
-        somsamp <- sort(msamp)
-        mmean <- mea(msamp)
-        mmin <- min(msamp)  
-        mmax <- max(msamp)  
-
-        i_mmean <- sum(somsamp<mmean) # place of mmean in the ordered list of elements
-        ci_upp <- somsamp[(i_mmean+level*1000/2)]
-        ci_low <- somsamp[(i_mmean-level*1000/2)]
-        CI <- c(ci_upp, ci_low)
-        
+        if (length(v)<1000){
+                print("This version expects a vector longer than 999" )  
+                return(NA)
+        }else if(level<=0 | level>=1){
+                print("This version expects  confidence level >0 and <1" )  
+                return(NA)
+        }else
+                CI <- NA
+                while(any(is.na(CI)) | length(CI)>2  ){
+                        msamp <- NA
+                        for (i in 1:1000) {
+                                samp <- sample(v,round(length(v)/1000))
+                                msamp[i] <- mea(samp) 
+                        }
+                        somsamp <- sort(msamp)
+                        mmean <- mea(msamp)
+                        mmin <- min(msamp)  
+                        mmax <- max(msamp)  
+                
+                        i_mmean <- sum(somsamp<mmean) # place of mmean in the ordered list of elements
+                        ci_upp <- somsamp[(i_mmean+level*1000/2)]
+                        ci_low <- somsamp[(i_mmean-level*1000/2)]
+                        CI <- c(ci_low,ci_upp)
+                }
+                
         return(CI)
-        
-}
+        }
 
-confintmean(v,0.999)
+####################
+
+confintmean(v,0.10)
+
+# left to do:
+# - correct std() to more exactly reproduce sd()
+# - research to chose the sampling parameter here set to 1000 ideally, treating vectors shorter than 1000.
+# - more conceptual treatment of the case when (i_mmean+level*1000/2) or i_mmean-level*1000/2) out of index range (currently restarting the whole process)
+
+# if to be used as a proper function, dealing with parameters of wrong data types, or out of range
+
+
+
+
+
+
+
+
+
+
+
 
